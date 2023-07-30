579d7f790778eb30dfb38f1850b0c9e06379b329
MarcosMoreno0912 <moreno83marcos@gmail.com>
2023-07-30 00:06:14 -0300

    add back button in ActivitiesPage

--- client/src/views/ActivitiesPage/ActivitiesPage.jsx
+++ client/src/views/ActivitiesPage/ActivitiesPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Notification from '../../components/Notification/Notification.jsx';
 import ActivityContainer from '../../components/ActivityContainer/ActivityContainer.jsx';
 import { clearDeleteSuccessActivity } from '../../redux/actions.js';
@@ -8,10 +9,15 @@ import style from './ActivitiesPage.module.css';
 const ActivitiesPage = () => {
 	const deleteSuccessActivity = useSelector(state => state.deleteSuccessActivity);
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
 	const handleNotificationClose = () => {
 		dispatch(clearDeleteSuccessActivity());
 	};
+
+	const handleBackClick = () => {
+		navigate('/home')
+	}
 
 	return (
 		<div className={style.activitiesPage}>
@@ -19,6 +25,9 @@ const ActivitiesPage = () => {
         		<Notification type="success" message="Actividad eliminada exitosamente" onClose={handleNotificationClose}/>
       		)}
 			<ActivityContainer />
+			<div className={style.buttonContainer} >
+				<button className={style.backButton} onClick={handleBackClick}>Back</button>
+			</div>
 		</div>
 	)
 }
