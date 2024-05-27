a1521e37c578cab239e7c203f80a834ba9364d12
Marcos <marcosdev24@gmail.com>
2024-05-27 15:34:54 -0300

    change postgres DB for mysql DB

--- server/src/db.js
+++ server/src/db.js
@@ -1,14 +1,15 @@
-require("dotenv").config();
+require('dotenv').config();
 const { Sequelize } = require("sequelize");
 
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
-  logging: false, 
+const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+  logging: false,
+  dialect: 'mysql', 
   native: false, 
 });
 
