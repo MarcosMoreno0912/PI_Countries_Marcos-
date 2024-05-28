85df92e8d0ef05a161f52f9270399ff6d97de147
Marcos <marcosdev24@gmail.com>
2024-05-28 18:58:45 -0300

    ajust and modify validations

--- client/src/views/FormPage/FormPageUtils.js
+++ client/src/views/FormPage/FormPageUtils.js
@@ -1,31 +1,40 @@
-export const validate = (formData) => {
+export const validate = (formData, touchedFields = {}) => {
 	const { name, difficulty, duration, season } = formData;
 	const newErrors = {};
 
-	if(!name) {
-		newErrors.name = 'Name is required';
-	}else if((name && name.length > 30) || (name && name.length < 3)){
-		newErrors.name = 'Name must be between 3 and 30 characters';
+	if(touchedFields.name){	
+		if(!name) {
+			newErrors.name = 'Nombre es requerido';
+		}else if((name && name.length > 30) || (name && name.length < 3)){
+			newErrors.name = 'Nombre debe tener entre 3 y 30 caracteres';
+		}
 	}	 
 
-	if(!difficulty) {
-		newErrors.difficulty = 'Difficulty is required';
-	}else if(difficulty < 1 || difficulty > 5) {
-		newErrors.difficulty = 'Difficulty must be between 1 and 5';
-	} 
+	if(touchedFields.difficulty){
+		if(!difficulty) {
+			newErrors.difficulty = 'Dificultad es requerida';
+		}else if(difficulty < 1 || difficulty > 5) {
+			newErrors.difficulty = 'Elija un valor entre 1 y 5';
+		} 
+	}
 			
-	if(!duration) {
-		newErrors.duration = 'Duration is required';
-	}else if(duration < 1 || duration > 24){
-		newErrors.duration = 'Duration must be between 1 and 24 hours';
+	if(touchedFields.duration){
+		if(!duration) {
+			newErrors.duration = 'Duracion es requerida';
+		}else if(duration < 1 || duration > 24){
+			newErrors.duration = 'Elija un valor entre 1 y 24 hs';
+		}
 	}
 
-	if(!season) {
-		newErrors.season = 'Season is required';
-	}else if(season !== 'Verano' && season !== 'Invierno' && season !== 'Otoño' && season !== 'Primavera'){
-		newErrors.season = 'Seasons: Verano, Invierno, Otoño y Primavera'
+	if(touchedFields.season){
+		if(!season) {
+			newErrors.season = 'Temporada es requerida';
+		}else if(season !== 'Verano' && season !== 'Invierno' && season !== 'Otoño' && season !== 'Primavera'){
+			newErrors.season = 'Opciones: Verano, Invierno, Otoño y Primavera'
+		}
 	}
 	 return newErrors;
+
 };
 
 export const resetForm = (setForm, setErrors) => {
